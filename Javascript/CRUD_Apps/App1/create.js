e76949edee703f8fd61ed31ef1be74759f8d2a84
vmurali100 <vmurali100@gmail.com>
2020-01-30 19:02:07 +0530

    Adding Some Changes

--- Javascript/CRUD_Apps/App1/create.js
+++ Javascript/CRUD_Apps/App1/create.js
@@ -1,30 +1,19 @@
-var users = [];
+var users = JSON.parse(sessionStorage.getItem("users"));
+if (users == null) {
+  users = [];
+}
+person = {
+  fname: "",
+  lname: "",
+  email: "",
+  gender: "",
+  subjects: []
+};
 function addUser() {
-  var person = {
-    fname: "",
-    lname: "",
-    email: "",
-    gender: ""
-  };
-
-  //Capturing all values from HTML in DOM method
-  person.fname = document.getElementById("fname").value;
-  person.lname = document.getElementById("lname").value;
-  person.email = document.getElementById("email").value;
-
-  //Capturing all values from HTML in DOM method in for loop
-
-  //   for (a in person) {
-  //     person[a] = document.getElementById(a).value;
-  //   }
-  var allRadioBtns = document.getElementsByName("gender");
-  allRadioBtns.forEach(function(element) {
-    if (element.checked) {
-      person.gender = element.value;
-    }
-  });
+  person = captureValues();
   users.push(person);
   displayAllUsers(users);
+  sessionStorage.setItem("users", JSON.stringify(users));
   clearForm(person);
 }
 
@@ -58,21 +47,52 @@ function displayAllUsers(data) {
   }
 }
 
-function clearForm(person) {
+function clearForm() {
   for (a in person) {
-    if (a !== "gender") {
+    if (a !== "gender" && a !== "subjects") {
       document.getElementById(a).value = "";
+      person[a] = "";
     } else if (a == "gender") {
-      var allRadio = document.getElementsByName("gender");
-      allRadio.forEach(function(element) {
+      allRadioBtns.forEach(function(element) {
+        element.checked = false;
+        person[a] = "";
+      });
+    } else if (a == "subjects") {
+      allCheckBoxes.forEach(element => {
         element.checked = false;
       });
     }
   }
+  validateForm();
 }
 
+function captureValues() {
+  //Capturing all values from HTML in DOM method
+  person.fname = document.getElementById("fname").value;
+  person.lname = document.getElementById("lname").value;
+  person.email = document.getElementById("email").value;
+
+  //Capturing all values from HTML in DOM method in for loop
+
+  //   for (a in person) {
+  //     person[a] = document.getElementById(a).value;
+  //   }
+  allRadioBtns = document.getElementsByName("gender");
+  allRadioBtns.forEach(function(element) {
+    if (element.checked) {
+      person.gender = element.value;
+    }
+  });
+
+  allCheckBoxes = document.getElementsByName("subject");
+  allCheckBoxes.forEach(function(element) {
+    if (element.checked) {
+      person.subjects.push(element.value);
+    }
+  });
+  return person;
+}
 function searchUser() {
-  console.log("searchUser called");
   var seearchText = document.getElementById("search").value;
   newUsers = users.filter(function(user) {
     return user.fname.indexOf(seearchText) > -1;
@@ -80,3 +100,24 @@ function searchUser() {
   console.log(newUsers);
   displayAllUsers(newUsers);
 }
+
+displayAllUsers(users);
+
+function validateForm() {
+  var check = null;
+  person = captureValues();
+  for (a in person) {
+    console.log(person[a]);
+    if (a != "subjects") {
+      if (person[a] != "") {
+        check = true;
+      }
+    }
+  }
+  if (check == null) {
+    document.getElementById("myBtn").setAttribute("disabled", true);
+  } else {
+    document.getElementById("myBtn").removeAttribute("disabled");
+  }
+}
+validateForm();
